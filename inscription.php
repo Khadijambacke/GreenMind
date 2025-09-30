2f2fe86247f75e804774f819a858029fdba885f5
khady <khadimbacke734@gmail.com>
2025-09-30 11:18:07 +0000

    modifications sur le scripts,js

--- inscription.php
+++ inscription.php
@@ -1,18 +1,23 @@
 <?php
 ///connexion avec la base etablie 
-  require_once 'configuration/config.php';
-  if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['submit'])) {
-    $nom = $_POST['nom'];
-    $email = $_POST['email'];
-
-    $sql = "INSERT INTO utilisateurs (nom, email) VALUES (:nom, :email)";
+require_once 'configuration/config.php';
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['inscrire'])) {
+  $nom = $_POST['nom'];
+  $prenom = $_POST['prenom'];
+  $email = $_POST['email'];
+  $passeword = password_hash($_POST['passeword'], PASSWORD_DEFAULT);
+  try {
+    $sql = "INSERT INTO utilisateurs (nomu, prenomu, mailu, motdepass) VALUES (:nom, :prenom, :email, :passeword)";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute([
-        ':nom' => $nom,
-        ':email' => $email
-    ]);
-
-    echo "Données insérées avec succès ✅";
+    $stmt->bindParam(':nom', $nom);
+    $stmt->bindParam(':prenom', $prenom);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':passeword', $passeword);
+    $stmt->execute();
+    ///echo "Nouvel étudiant ajouté avec succès !";
+  } catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+  }
 }
 
 
@@ -20,6 +25,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -47,24 +53,24 @@
         <!-- Formulaire -->
         <div class="col-md-6 p-4">
           <h3>Inscription</h3>
-          <form action="inscription.php"  method="POST">
-            <div class="mb-3">
-              <label>Prenom</label>
-              <input type="Prenom" class="form-control" required>
-            </div>
+          <form action="" method="POST">
             <div class="mb-3">
               <label>Nom</label>
-              <input type="Nom" class="form-control" required>
+              <input type="Nom" name="nom" class="form-control" required>
+            </div>
+            <div class="mb-3">
+              <label>Prenom</label>
+              <input type="Prenom" name="prenom" class="form-control" required>
             </div>
             <div class="mb-3">
               <label>Email</label>
-              <input type="email" class="form-control" required>
+              <input type="email" name="email" class="form-control" required>
             </div>
             <div class="mb-3">
               <label>Mot de passe</label>
-              <input type="password" class="form-control" required>
+              <input type="password" name="passeword" class="form-control" required>
             </div>
-            <button name="submit" id="submit" type="submit" class="btn btn-primary w-100" style="background-color: #88268C;" >S'inscrire</button>
+            <button id="submit" type="submit" name="inscrire" class="btn btn-primary w-100" style="background-color: #88268C;">S'inscrire</button>
           </form>
         </div>
       </div>
--- ressources/scripts.js
+++ ressources/scripts.js
@@ -1,6 +1,6 @@
 /*ici on fait de tel sorte que l'orsqu'on appuit sur submit il affiche uun alert pour nous dire qu'on s'est crer un compte
 et nous redirige vers se conecter(DOMContentLoaded pour que tout l'html se charge d'abord, 
-event.preventDefault()pour ne pas reactualiser la page donc l'alert ne s'afficherais pas ) */
+event.preventDefault()pour ne pas reactualiser la page donc l'alert ne s'afficherais pas )     */
 document.addEventListener("DOMContentLoaded", function() {
     let seconte=document.getElementById("submit");
     seconte.onclick = function(event) {
